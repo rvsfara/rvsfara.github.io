1da23437b48e4d049bb57c5fc361e1566ac30ad3
Rafael Vieira Dos Santos <rvsfara@gmail.com>
2022-10-12 18:00:03 -0300

    linha do Tempo UX

--- scripts/menuBar.js
+++ scripts/menuBar.js
@@ -1,22 +1,26 @@
 //Obtem a referencia ao objeto da div 
+dvImgPerfil  = document.getElementById("dvImgPerfil");
 dvInformacaoPessoal = document.getElementById("dvInformacaoPessoal");
 dvFormacao = document.getElementById("dvFormacao");
 dvExperiencia = document.getElementById("dvExperiencia");
 dvCursos  = document.getElementById("dvCursos");
+dvLinha  = document.getElementById("dvLinha");
 //esconde todas as informações para que o usuário possa selecionar o que exibir
-escondeTudo();
 //exibir a página de cursos
-funcaoCursos();
+funcaoLinhaTempo();
 //função para ocultar a visualização facilitando a visualização
 function escondeTudo(){
 	dvInformacaoPessoal.style.display = "none";
 	dvFormacao.style.display = "none";
 	dvExperiencia.style.display = "none";
 	dvCursos.style.display = "none";
+	dvLinha.style.display = "none";
+	dvImgPerfil.style.display = "none";
 	
 }
 function funcaoDadosPessoais(){
 	escondeTudo();
+	dvImgPerfil.style.display = "block";
 	dvInformacaoPessoal.style.display = "block";
 }
 function funcaoFormacao(){
@@ -33,4 +37,8 @@ function funcaoCursos(){
 }
 function funcaoProjetos(){
 	escondeTudo();
+}
+function funcaoLinhaTempo(){
+	escondeTudo();
+	dvLinha.style.display = "block";
 }
